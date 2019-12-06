584274017c2d2b8ba1de1654f87671dcfe430500
Kanchan Chauhan <angel143.dr@gmail.com>
2019-12-06 11:54:09 +0530

    refactor(Dunning): Print and other fixes

--- dunning/dunning/doctype/dunning/dunning.py
+++ dunning/dunning/doctype/dunning/dunning.py
@@ -5,6 +5,21 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+import json
+from six import string_types
 
 class Dunning(Document):
 	pass
+
+@frappe.whitelist()
+def get_text_block(dunning_type, doc):	
+	"""
+		This allows the rendering of parsed fields in the jinja template
+	"""
+	if isinstance(doc, string_types):
+		doc = json.loads(doc)
+
+	text_block = frappe.db.get_value('Dunning Type Text Block', {'parent': dunning_type}, 'text_block')
+
+	if text_block:
+		return frappe.render_template(text_block, doc)
