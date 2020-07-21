c7035771b36a3d5940854f6ea5345170c24926c2
jbienesdev <jbienes.dev@gmail.com>
2020-07-21 09:48:52 +0800

    fix(dunning_list): change filter name

--- dunning/dunning/doctype/dunning/dunning_list.js
+++ dunning/dunning/doctype/dunning/dunning_list.js
@@ -2,10 +2,10 @@ frappe.listview_settings['Dunning'] = {
 	add_fields: ["resolution_status"],
 	get_indicator: function (doc) {
 		if (doc.resolution_status === "Resolved") {
-			return [__("Resolved"), "green", "status,=,Resolved"];
+			return [__("Resolved"), "green", "resolution_status,=,Resolved"];
 		}
 		else {
-			return [__("Unresolved"), "yellow", "status,=,Unresolved"];
+			return [__("Unresolved"), "yellow", "resolution_status,=,Unresolved"];
 		}
 	}
 }
